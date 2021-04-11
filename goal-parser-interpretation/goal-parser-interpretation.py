0c2217c6f4d5d8449234b70e64d55a36dba1f0aa
Shaurya Choudhary <shaurya.src@gmail.com>
2021-04-11 20:35:58 +0530

    Time: 28 ms (77.97%), Space: 14.2 MB (39.87%) - LeetHub

---
+++ goal-parser-interpretation/goal-parser-interpretation.py
@@ -0,0 +1,18 @@
+class Solution:
+    def interpret(self, command: str) -> str:
+        result = ''
+        i = 0
+        while i < len(command):
+            if command[i] == 'G':
+                result += 'G'
+                i += 1
+            elif command[i] == '(':
+                if command[i+1] == ')':
+                    result += 'o'
+                    i += 2
+                else:
+                    result += command[i+1:i+3]
+                    i += 4
+                
+                
+        return result
