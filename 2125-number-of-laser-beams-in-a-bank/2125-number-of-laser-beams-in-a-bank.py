519e2134b50afa8625bd0ad1210dcafbdd797080
Shaurya Choudhary <shaurya.src@gmail.com>
2022-04-16 13:31:08 +0530

    Time: 190 ms (47.27%), Space: 16 MB (92.21%) - LeetHub

---
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.py
@@ -0,0 +1,16 @@
+class Solution:
+    def numberOfBeams(self, bank: List[str]) -> int:
+        total_beams = 0
+        curr_row, last_row = 0, 0
+        
+        for row in bank:
+            curr_row = 0
+            for ch in row:
+                if ch == '1':
+                    curr_row += 1
+                    
+            total_beams += last_row * curr_row
+            if curr_row > 0:
+                last_row = curr_row
+        return total_beams
+                
