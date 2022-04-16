8db6f14d648f583cfe7a23ddfa49390a7ddfaab6
Shaurya Choudhary <shaurya.src@gmail.com>
2022-04-16 13:43:12 +0530

    Time: 177 ms (49.07%), Space: 16 MB (92.21%) - LeetHub

--- 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.py
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.py
@@ -4,11 +4,7 @@ class Solution:
         curr_row, last_row = 0, 0
         
         for row in bank:
-            curr_row = 0
-            for ch in row:
-                if ch == '1':
-                    curr_row += 1
-                    
+            curr_row = len([1 for ch in row if ch == '1'])
             total_beams += last_row * curr_row
             if curr_row > 0:
                 last_row = curr_row
